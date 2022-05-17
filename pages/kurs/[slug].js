4ca9f6a93eb6d56dd43fd34647431b3dcdafb0e7
lukeperrin10 <lukeperrin27@gmail.com>
2022-05-17 12:33:15 +0200

    adds option to see start date for courses

--- pages/kurs/[slug].js
+++ pages/kurs/[slug].js
@@ -1,7 +1,11 @@
-import getAllProducts from '../../lib/get-all-product';
-import getProductBySlug from '../../lib/get-product-slug';
+import getAllProducts from '../../lib/get-all-product'
+import getProductBySlug from '../../lib/get-product-slug'
 
 const Product = ({ product }) => {
+  let dates = product.dates.map((date) => {
+    return <option>{date}</option>
+  })
+
   return (
     <div>
       <h1>{product.name}</h1>
@@ -10,38 +14,40 @@ const Product = ({ product }) => {
         <br />
         {product.price} SEK
         <br />
+        Course Start dates: <select>{dates}</select>
+        <br />
         Course Holder: {product.variants[0].name}
       </p>
       <img src={product.variants[0].courseLeaderImage.url}></img>
     </div>
-  );
-};
+  )
+}
 
 export async function getStaticPaths() {
-  let paths = [];
+  let paths = []
 
-  const { products } = await getAllProducts();
+  const { products } = await getAllProducts()
   paths = [
     ...paths,
     ...products.map((product) => ({
       params: { slug: product.slug },
     })),
-  ];
+  ]
 
   return {
     paths,
     fallback: false,
-  };
+  }
 }
 
 export async function getStaticProps({ params }) {
-  const { product } = await getProductBySlug({ slug: params.slug });
+  const { product } = await getProductBySlug({ slug: params.slug })
 
   return {
     props: {
       product,
     },
-  };
+  }
 }
 
-export default Product;
+export default Product
