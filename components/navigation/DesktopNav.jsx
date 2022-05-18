3caf5cd194c465981816bde4109e599137c41438
lukeperrin10 <lukeperrin27@gmail.com>
2022-05-18 15:17:25 +0200

    fixes navigation to match routes on current website

---
+++ components/navigation/DesktopNav.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import Link from 'next/link'
+import MobNav from './MobNav'
+
+
+const DesktopNav = () => {
+  const [mobNav, setMobNav] = useState(false)
+  const Toggle = () => setMobNav(!mobNav)
+  const [scrollY, setScrollY] = useState(0)
+  const router = useRouter()
+
+  const location = router.asPath
+  const homeActive = ['/'].includes(location) ? 'active' : ''
+  const propertiesActive = ['/fastigheter'].includes(location) ? 'active' : ''
+  const aboutActive = ['/om-oss'].includes(location) ? 'active' : ''
+  const contactActive = ['/kontakta-oss'].includes(location) ? 'active' : ''
+
+  const logit = () => {
+    setScrollY(window.pageYOffset)
+  }
+
+  useEffect(() => {
+    const watchScroll = () => {
+      window.addEventListener('scroll', logit)
+    }
+    watchScroll()
+    return () => {
+      window.removeEventListener('scroll', logit)
+    }
+  })
+
+  const isScrolled = scrollY >= 10 ? 'scrolled' : ''
+  return (
+    <div className={`navbar ${isScrolled}`}>
+      <div className='navbar__image'>
+        <Link href='/' passHref>
+          <a>
+            <Image
+              priority
+              placeholder='empty'
+              src='/batteri.svg'
+              height={121}
+              width={388}
+              as='img/svg'
+              alt='Batteri logo'
+            />
+          </a>
+        </Link>
+      </div>
+      <button className='navbar__burger' onClick={() => Toggle()}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <MobNav show={mobNav} close={Toggle} />
+
+      <div className='navbar__links'>
+        <div className='navbar__links--link'>
+          <Link href='/' passHref>
+            <a className={`navbar__links--a ${homeActive}`}>Hem</a>
+          </Link>
+        </div>
+        <div className='navbar__links--link'>
+          <Link href='/kurser' passHref>
+            <a className={`navbar__links--a ${propertiesActive}`}>
+             Kurser
+            </a>
+          </Link>
+        </div>
+        <div className='navbar__links--link'>
+          <Link href='/event' passHref>
+            <a className={`navbar__links--a ${aboutActive}`}>Event</a>
+          </Link>
+        </div>
+        <div className='navbar__links--link'>
+          <Link href='/kontakta-oss' passHref>
+            <a className={`navbar__links--a ${contactActive}`}>Kontakt</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DesktopNav
--- components/navigation/Nav.jsx
+++ components/navigation/Nav.jsx
@@ -1,49 +1,12 @@
-import React, { useState } from 'react'
-import Link from 'next/link'
+
+import DesktopNav from './DesktopNav'
 
 const Nav = () => {
-  const [nav, setNav] = useState(false)
-  const Toggle = () => setNav(!nav)
-  const isOpen = nav ? 'is-open' : ''
 
-  return (
-    <>
-      <a className={`btn-burger ${isOpen}`} onClick={() => Toggle()}></a>
 
-      <div className={`side-menu ${isOpen}`}>
-        <ul className='listItems'>
-          <li>
-            <Link href='/'>
-              <a className='nav-link nav-link--1' onClick={() => Toggle()}>
-                Hem
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/kurser'>
-              <a className='nav-link nav-link--2' onClick={() => Toggle()}>
-                Kurser
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/event'>
-              <a className='nav-link nav-link--3' onClick={() => Toggle()}>
-                Event
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/kontakt'>
-              <a className='nav-link nav-link--4' onClick={() => Toggle()}>
-                Kontakt
-              </a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </>
-  )
+  return <DesktopNav />
 }
 
 export default Nav
+
+
