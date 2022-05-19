7b58ae6ba0843b9cee71e42704b1d6bbc355258e
lukeperrin10 <lukeperrin27@gmail.com>
2022-05-20 00:39:16 +0200

    adds handler for course duration and drop down list of coming courses

---
+++ modules/helper.js
@@ -0,0 +1,58 @@
+const HopHelper = {
+  addCourseDuration(product) {
+    let courseDuration = product.courseDuration[0]
+    let dates = product.dates.map((date, index) => {
+      return new Date(date)
+    })
+
+    let endDay = (courseDate) => {
+      let day = '' + courseDate.getDay()
+      let lastDay = parseInt(day)
+
+      let finalDay = (lastDay += courseDuration)
+
+      let string = finalDay.toString()
+
+      if (string.length < 2) {
+        string = '0' + string
+      }
+
+      return string
+    }
+
+    let startDate = dates.map((courseDate, index) => {
+      let day = '' + courseDate.getDay()
+      let month = '' + (courseDate.getMonth() + 1)
+      let year = courseDate.getFullYear()
+
+      if (day.length < 2) {
+        day = '0' + day
+      }
+
+      if (month.length < 2) {
+        month = '0' + month
+      }
+
+      return [year, month, day].join('-')
+    })
+
+    let endDate = dates.map((courseDate, index) => {
+      let month = '' + (courseDate.getMonth() + 1)
+      let year = courseDate.getFullYear()
+
+      if (month.length < 2) {
+        month = '0' + month
+      }
+
+      return [year, month, endDay(courseDate)].join('-')
+    })
+    let options = startDate.map(function (e, i) {
+      return [e, endDate[i]].join(' - ')
+    })
+    return options.map((option) => {
+      return <option>{option}</option>
+    })
+  },
+}
+
+export default HopHelper
