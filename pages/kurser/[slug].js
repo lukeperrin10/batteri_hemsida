d3bcbaa0fa52bbf19691269369c1f812512ee97a
lukeperrin10 <lukeperrin27@gmail.com>
2022-05-31 09:56:20 +0200

    fixes Category page to send correct info to card component

--- pages/kurser/[slug].js
+++ pages/kurser/[slug].js
@@ -6,11 +6,22 @@ import getCategoryBySlug from '../../lib/get-category-slug'
 import Card from '../../components/Card'
 
 const CategoryPage = ({ category }) => {
-  return (
-    <>
-      <Card data={category} />
-    </>
-  )
+  let product_data = category.products?.map((product) => product)
+  let image = category.products?.[0].images[0]
+  let wideImage = category.products?.[0].wideImage
+  let logo = category.products?.[0].logo
+  const data = {
+    slug: product_data[0].slug,
+    gradientColor: product_data[0].gradientColor,
+    isWide: product_data[0].isWide,
+    image: image,
+    wideImage: wideImage,
+    name: product_data[0].name,
+    logo: logo,
+    description: product_data[0].description,
+  }
+
+  return <Card data={data} />
 }
 
 export async function getStaticPaths() {
