04348aaf3f36ee325263ba865eda06a9a4d10431
kim_haaga <kim.haaga@live.se>
2022-06-16 12:49:10 +0200

    Finnish up desktop version

--- components/CourseLeader.jsx
+++ components/CourseLeader.jsx
@@ -5,43 +5,82 @@ import { styled } from '../stitches.config'
 const CourseLeaderSection = styled('section', {
   display: 'flex',
   justifyContent: 'center',
-  backgroundColor: '$greyBg',
+  alignItems: 'center',
+
   width: '100vw',
+  maxWidth: '2560px',
+  height: 'fit-content',
+  variants: {
+    variant: {
+      mobile: {
+        rowGap: 13,
+        flexDirection: 'column',
+      },
+      desktop: {
+        columnGap: 140,
+        flexDirection: 'row',
+      },
+    },
+  },
 })
 
 const NameAndImage = styled('div', {
   display: 'flex',
-
+  alignItems: 'center',
+  columnGap: 43,
 })
 const ImageContainer = styled('div', {
+  position: 'relative',
 
+  span: {
+    borderRadius: '50%',
+  },
+  variants: {
+    variant: {
+      mobile: {
+        square: 130,
+      },
+      desktop: {
+        square: 240,
+      },
+    },
+  },
 })
 const NameContainer = styled('div', {
   display: 'flex',
-
+  flexDirection: 'column',
 })
 const Title = styled('p', {
-
+  color: '$black',
+  fontWeight: '$bold',
+  fontSize: '$3',
+  marginBottom: 10,
 })
 const Name = styled('p', {
-
+  color: '$black',
+  fontWeight: '$bold',
+  fontSize: '$5',
 })
-const About = styled('p', {
-
+const About = styled('div', {
+  maxWidth: 670,
+  p: {
+    color: '$black',
+  },
+  a: {
+    color: '$blueLink',
+    '&:hover': {
+      color: '$blueDark',
+    },
+  },
 })
 
 const CourseLeader = ({ data }) => {
   const { courseLeaderAbout, courseLeaderImage, name, profession } = data[0]
   return (
-    <CourseLeaderSection>
+    <CourseLeaderSection variant={{ '@initial': 'mobile', '@bp7': 'desktop' }}>
       <NameAndImage>
-        <ImageContainer>
-          <Image
-            src={courseLeaderImage.url}
-            alt=''
-            height={40}
-            width={40}
-          />
+        <ImageContainer variant={{ '@initial': 'mobile', '@bp3': 'desktop' }}>
+          <Image src={courseLeaderImage.url} alt='' layout='fill' />
         </ImageContainer>
         <NameContainer>
           <Title>Kurs ledare</Title>
@@ -49,7 +88,7 @@ const CourseLeader = ({ data }) => {
           <Name>{profession}</Name>
         </NameContainer>
       </NameAndImage>
-      <About  dangerouslySetInnerHTML={{ __html: courseLeaderAbout.html }}/>
+      <About dangerouslySetInnerHTML={{ __html: courseLeaderAbout.html }} />
     </CourseLeaderSection>
   )
 }
