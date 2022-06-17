d870c51ddaa5aa92d57a81544f243509a22de330
kim_haaga <kim.haaga@live.se>
2022-06-17 11:13:33 +0200

    Add icon grid to contact us

--- pages/kontakta-oss.js
+++ pages/kontakta-oss.js
@@ -1,8 +1,73 @@
 import React from 'react'
 import getPageData from '../lib/get-page-data'
 import getAllAktuellts from '../lib/get-all-aktuellts'
-
+import { styled } from '../stitches.config'
 import CourseHero from '../components/CourseHero'
+import Image from 'next/image'
+
+const IconGrid = styled('div', {
+  width: '100vw',
+  maxWidth: 2560,
+  display: 'flex',
+  justifyContent: 'center',
+  flexWrap: 'wrap',
+  columnGap: 100,
+  variants: {
+    variant: {
+      mobile: {
+        marginTop: 40,
+        flexDirection: 'column',
+        alignItems: 'center',
+        rowGap: 75,
+      },
+      desktop: {
+        marginTop: 125,
+        flexDirection: 'row',
+        rowGap: 100,
+      },
+    },
+  },
+})
+const IconGridItem = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: 300,
+  height: 'fit-content',
+  variants: {
+    variant: {
+      mobile: {},
+      desktop: {},
+    },
+  },
+})
+const IconBox = styled('div', {
+  position: 'relative',
+  square: 220,
+  variants: {
+    variant: {
+      mobile: {},
+      desktop: {},
+    },
+  },
+})
+const ItemText = styled('p', {
+  textAlign: 'center',
+  color: '$black',
+  variants: {
+    variant: {
+      mobile: {},
+      desktop: {},
+    },
+  },
+})
+
+const Divider = styled('div', {
+  height: 1,
+  border: '1px solid $grey',
+  marginY: 125,
+  width: '70%',
+})
 
 const ContactUs = () => {
   const heroData = {
@@ -18,9 +83,40 @@ const ContactUs = () => {
       },
     },
   }
+
+  const iconGridData = [
+    {
+      id: 1,
+      text: 'Lorem imsum dolerm irum veni',
+      icon: '/check.svg',
+    },
+    {
+      id: 2,
+      text: 'Lorem imsum dolerm irum veni vineLorem imsum dole',
+      icon: '/batteri.svg',
+    },
+    {
+      id: 3,
+      text: 'Lorem imsum dolerm irum veni vineLorem',
+      icon: '/check.svg',
+    },
+  ]
+
+  const items = iconGridData.map((item, id) => {
+    return (
+      <IconGridItem key={id}>
+        <IconBox>
+          <Image src={item.icon} alt='' layout='fill' />
+        </IconBox>
+        <ItemText>{item.text}</ItemText>
+      </IconGridItem>
+    )
+  })
   return (
     <>
-      <CourseHero data={heroData} btnText={'Begär Offert'}/>
+      <CourseHero data={heroData} btnText={'Begär Offert'} />
+      <IconGrid variant={{ '@initial': 'mobile', '@bp3': 'desktop' }} >{items}</IconGrid>
+      <Divider variant={{ '@initial': 'mobile', '@bp3': 'desktop' }} />
     </>
   )
 }
